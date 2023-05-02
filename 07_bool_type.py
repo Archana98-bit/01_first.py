e0e057e02a1bbe8a921144cfa3f7650144ea5f77
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:29:34 +0530

    Add files via upload

---
+++ 07_bool_type.py
@@ -0,0 +1,16 @@
+a=True
+print(type(a))
+print(a)
+
+x=False
+print(type(x))
+print(x)
+
+a=67
+b=89
+c=a<b
+print(c)
+
+print(True+True+True+True+False+True)
+
+print(True+True+True+True+True+True/True+False+True)
