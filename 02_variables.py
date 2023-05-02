a450bf0baa0e79caab0279c124795eb3faf3b77a
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:26:38 +0530

    Add files via upload

---
+++ 02_variables.py
@@ -0,0 +1,31 @@
+# a=105
+# print(a)
+# print(a*5)
+# print(a/5)
+
+# Add two numbers.
+
+num1=65
+num2=85
+result=num1+num2
+print(result)
+
+# Substract two numbers.
+
+num1=78
+num2=35
+result=num1-num2
+print(result)
+
+#Multiply two numbers.
+num1=23
+num2=22
+result=num1*num2
+print(result)
+
+#Divide two numbers.
+
+num1=86
+num2=2
+result=num1/num2
+print(result)
