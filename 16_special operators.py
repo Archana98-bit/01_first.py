7ee4ac4971eab14cbcdce04665c458531291a584
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:34:41 +0530

    Add files via upload

---
+++ 16_special operators.py
@@ -0,0 +1,29 @@
+a=890
+b=890
+print(a is b)
+print(id(a))
+print(id(b))
+
+a=789
+b=876
+print(a is b)
+print(a is not b)
+print(id(a))
+print(id(b))
+
+name='archana'
+name1='archana'
+print(name is name1)
+
+a=True
+b=False
+print(a is not b)
+
+name="archana pati"
+print('h' in name)
+print('hh' in name)
+print('' in name)
+print('e' not in name)
+
+list=['priyanka','rahul','zini','jack']
+print('zini' in list)
