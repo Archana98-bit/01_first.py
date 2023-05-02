38a0c1c4fc90128a76832cab5404a5f36ea257c5
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:31:10 +0530

    Add files via upload

---
+++ 10_TypeCasting.py
@@ -0,0 +1,126 @@
+# Other to int.
+# float to int.
+
+x=int(78.60)
+print(x)
+
+# Complex to int.
+
+a=int(57+34j)
+print(a)
+
+# Bool to int.
+
+a=int(True)
+print(a)
+
+x=int(False)
+print(x)
+
+#String to int.
+
+a=('Welcome')
+print(a)
+y=('500000')
+print(y)
+
+# Other to float.
+# int to float.
+
+a=float(466)
+print(a)
+
+# Complex to float.
+
+x=float(20+7j)
+print(x)
+
+# Bool to float.
+
+a=float(True+True)
+print(a)
+y=float(False+False-True)
+print(y)
+
+# String to float.
+a=float('hello')
+print(a)
+x=float('1000')
+print(x)
+c=float('555.78')
+print(c)
+
+#Other to complex.
+# int to complex.
+
+a=complex(67)
+print(a)
+
+# float to complex.
+a=complex(43.76)
+print(a)
+
+# Bool to complex.
+a=complex(True+True+False)
+print(a)
+
+# String to complex.
+a=complex('hello world')
+print(a)
+x=complex('455.89')
+print(x)
+y=complex(5,7)
+print(y)
+
+# other to bool.
+# int to bool.
+
+a=bool(1)
+print(a)
+
+x=bool(18)
+print(x)    #non zero value
+
+y=bool(0)
+print(y)
+
+# float to bool.
+a=bool(20.0)
+print(a)
+
+x=bool(0.0)
+print(x)
+a=bool(0.1)
+print(a)
+
+# complex to bool.
+a=bool(8+7j)
+print(a)
+b=bool(10-7j)
+print(b)
+
+# String to bool.
+a=bool('welcome')
+print(a)
+b=bool('')
+print(b)
+c=bool('100')
+print(c)
+
+# Other to string.
+#int to string.
+
+a=str(300)
+print(a)
+
+# float to string.
+a=str(10.50)
+print(a)
+
+# complex to string.
+a=str(75+8j)
+print(a)
+
+# bool to string.
+x=str(True+True+False)
+print(x)
