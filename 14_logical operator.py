670727e251f90146bdd055c7ed06f3e599a2fbe4
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:33:30 +0530

    Add files via upload

---
+++ 14_logical operator.py
@@ -0,0 +1,24 @@
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+#non-boolean type.
+
+print(not True)
+print(not False)
+
+print(45 and 89)
+print(500 or 678)
+print(not 698)
+print(0 or 57)
+print(0 and 78)
+print(not 'Rahul')
+print('Archana' and 'Archana pati')
+print('archana' or 'archanapati')
+print(not '')
