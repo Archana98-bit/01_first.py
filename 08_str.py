477a304e6e0d56abc99f1d3b2bda9fd0e9fc572a
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:30:09 +0530

    Add files via upload

---
+++ 08_str.py
@@ -0,0 +1,22 @@
+topic='Welcome to python'
+print(topic)
+
+msg="How are you!"
+print(msg)
+
+msg='''Here
+I will
+practice'''
+print(msg)
+
+addr="""Laxmi Nagar
+New Delhi
+India"""
+print(addr)
+
+msg="Welcome to 'python' programming language"
+print(msg)
+
+s1="how is your day going!"
+print(s1[7])
+print(s1[-4])
