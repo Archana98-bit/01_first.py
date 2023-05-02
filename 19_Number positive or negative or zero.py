8629d3137944ff34141033c91a4df9345ea28ee4
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:36:54 +0530

    Add files via upload

---
+++ 19_Number positive or negative or zero.py
@@ -0,0 +1,12 @@
+# WAP input a number and check that number is positive or negative or zero.
+
+num=int(input('enter a number'))
+
+if num==0:
+    print('Number is Zero')
+
+elif num>0:
+    print('Number is Positive')
+
+else:
+    print('Number is Negative')
