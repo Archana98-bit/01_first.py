d67bb6df988c3b32bc72d916f2f956c4febd674c
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:32:29 +0530

    Add files via upload

---
+++ 12_range.py
@@ -0,0 +1,18 @@
+r=range(15)
+for i in r:
+    print(i)
+
+r=range(5,15)
+for i in r:
+    print(i)
+
+r=range(6,10,2)
+for i in r:
+    print(i)
+
+r=range(14)
+print(r[5])
+r[5]=20
+print(r[5])
+
+print(r[20])
