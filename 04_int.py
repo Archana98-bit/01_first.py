e33d679505fa80b05955d92f6740ff26160facfa
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:28:28 +0530

    Add files via upload

---
+++ 04_int.py
@@ -0,0 +1,39 @@
+a=78
+print(type(a))
+
+x=-38
+print(type(x))
+
+y=60.5
+print(type(y))
+
+# convert decimal 13 into binary form.
+# bin(13)
+print(bin(13))
+
+# convert octal to binary form.
+# bin((0o163))
+print(bin(0o163))
+
+# convert decimal to octal form.
+# oct(70)
+print(oct(70))
+
+# convert binary to octal form.
+# oct(0b1111)
+print(oct(0b1111))
+
+# convert decimal to hexadecimal form.
+# hex(1000)
+print(hex(1000))
+
+# octal to hexadecimal form.
+# hex(0o365)
+print(hex(0o365))
+
+
+
+
+#Complex data type.
+x=54+89j
+print(type(x))
