d106ecf7222d478aa6b2bacdc9d7de3475d0ed33
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:35:15 +0530

    Add files via upload

---
+++ 17_Dynamic input.py
@@ -0,0 +1,18 @@
+# static input value.
+
+a=90
+b=89
+c=a+b
+print(c)
+
+# Dynamic input.
+
+a=input('enter a value')
+b=input('enter b value')
+c=a+b
+print(c)
+
+a=int(input('enter a value'))
+b=int(input('enter b value'))
+c=a+b
+print(c)
