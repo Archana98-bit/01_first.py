4c8c4e37d48ba6e4a8b7fc2d854a37f139166389
Archana98-bit <123820565+Archana98-bit@users.noreply.github.com>
2023-05-03 01:29:07 +0530

    Add files via upload

---
+++ 06_complex_data.py
@@ -0,0 +1,28 @@
+x=67+78j
+print(type(x))
+print(x)
+
+a=65.88+79j
+print(a)
+print(type(a))
+
+# real part---> bin/oct/hex/dec
+# imaginary---> dec
+
+x=0b1101+110j
+print(x)
+
+a=0o765+55j
+print(a)
+
+b=0x777de+45j
+print(b)
+
+a=1000+69j
+b=500000+78j
+c=a+b
+print(c)
+
+x=20+7j
+print(a.real)
+print(a.imag)
