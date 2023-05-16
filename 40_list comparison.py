5b2015403388829f8da3a0aaafe5673c4c856772
Archana Pati <123820565+Archana98-bit@users.noreply.github.com>
2023-05-16 21:41:20 +0530

    Add files via upload

---
+++ 40_list comparison.py
@@ -0,0 +1,41 @@
+# 1)
+l = ['priyanka', 'rahul', 'zini', 'jack']
+s = ['Priyanka', 'Rahul', 'Zini', 'Jack']
+p = ['priyanka', 'rahul', 'zini', 'jack']
+
+print(l==s)
+print(l==p)
+
+
+# 2)
+l = [10, 20, 30, 40, 50]
+s = [11, 22, 33, 44, 55]
+
+print(l!=s)
+
+
+# 3)
+l = [11, 22, 33, 44]
+s = [33, 44, 22, 11]
+
+print(l==s)
+print(l!=s)
+
+# 4)
+l = ['Surendra', 'priyanka', 'rahul', 'zini']
+s = ['surendra', 'Priyanka', 'Rahul', 'zini']
+a = [10, 20, 30, 40]
+b = [11, 22]
+
+print(l>s)
+print(a>b)
+
+print(l<s)
+print(a<b)
+
+print(l<=s)
+print(a<=b)
+
+print(l>=s)
+print(a>=b)
+
