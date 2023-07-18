f307b4c9b3548acc553a182738ef697ce0e27735
Archana Pati <123820565+Archana98-bit@users.noreply.github.com>
2023-07-18 20:43:53 +0530

    Add files via upload

---
+++ 91_math module.py
@@ -0,0 +1,59 @@
+# math module :
+# 1)
+import math
+print(help(math))
+print(dir(math))
+
+# 2)
+print(math.pi)
+
+# area of a circle.
+
+area = math.pi*3.9*3.9
+print(area)
+
+
+# 3)
+print(math.e)
+
+# working with different functions.
+# a)
+print(math.ceil(2.8))
+print(math.ceil(2.1))
+print(math.ceil(2.9))
+print(math.ceil(3))
+
+
+# b)
+print(math.floor(2.1))
+print(math.floor(2.5))
+print(math.floor(2.9))
+print(math.floor(2))
+print(math.floor(3))
+
+
+# c)
+print(math.sqrt(236))
+print(math.sqrt(144))
+print(math.sqrt(169))
+print(math.floor(math.sqrt(236)))
+
+
+# d)
+print(math.factorial(5))
+
+
+# e)
+print(math.fabs(-6))
+print(math.fabs(6))
+print(math.fabs(-2.8))
+
+
+# f)
+print(math.fmod(20,5))
+print(math.fmod(20,9))
+
+# g)
+print(math.fsum([100,200,300,400]))
+
+
