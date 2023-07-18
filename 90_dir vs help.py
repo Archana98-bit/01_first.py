1585d1ad9ae24ad450b63cf58c69cb6410e4e128
Archana Pati <123820565+Archana98-bit@users.noreply.github.com>
2023-07-18 20:43:22 +0530

    Add files via upload

---
+++ 90_dir vs help.py
@@ -0,0 +1,28 @@
+# dir() vs help() :
+# dir() function :
+
+import math
+#print(dir(math))
+import os
+#print(dir(os))
+import numpy
+#print(dir(numpy))
+import pandas
+#print(dir(pandas))
+
+# dir() functions will give only the members name.
+
+# help() function :
+
+#help(math)
+#help(os)
+#help(numpy)
+#help(pandas)
+
+# help() function is used to give you all the members name along with description
+
+
+import module1
+#print(dir(module1))
+
+help(module1)
