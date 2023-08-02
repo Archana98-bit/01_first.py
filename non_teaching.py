5e1f5d6a9d9c25acfdf93693705df12e013a7ae2
Archana Pati <123820565+Archana98-bit@users.noreply.github.com>
2023-08-02 22:01:50 +0530

    Add files via upload

---
+++ non_teaching.py
@@ -0,0 +1,2 @@
+def non_teaching_staffs() :
+    print("This is nonteaching.py module present inside staffs subpackages")
---
+++ teaching.py
@@ -0,0 +1,4 @@
+def teaching_staffs() :
+    print("This is teaching.py module present inside staffs subpackages")
+
+no_of_staffs = 130
